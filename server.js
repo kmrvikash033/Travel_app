e87d0c0388d41d51ea47acb5cace7a5d47435454
Vikash <kmr.vikash033@gmail.com>
2023-09-20 18:37:03 +0530

    fix: resolved cors issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const  express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const hotelDAtaAddedToDBRouter = require("./routes/dataimport.router")
@@ -16,6 +17,7 @@ const wishlistRouter = require("./routes/wishlist.router");
 const connectDB = require("./config/dbconfig");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 connectDB();
